a6cae922faca46d77e9972dd29b79b101bb2a87c
nairobi <glimglam@protonmail.com>
2025-04-25 21:07:06 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# chunithm-grapher
+
+This is a proof-of-concept hook tracking projected score (SCORE-) over time, in the touch slide game.
---
+++ dllmain.cpp
@@ -0,0 +1,110 @@
+#include "pch.h"
+#include <cstdlib>
+#include <cstdint>
+#include <cstring>
+#include <fstream>
+#include <sstream>
+#include <TlHelp32.h>
+#include <detours/detours.h>
+
+int32_t g_current_score = 0;
+
+int32_t(__fastcall* OrigGetLostScore)(void* arg) = (int32_t(__fastcall*)(void*))0x00bee430;
+static int32_t __fastcall GetLostScore(void* arg)
+{
+      g_current_score = (*OrigGetLostScore)(arg);
+      return g_current_score;
+}
+
+static uintptr_t GetModuleBaseAddress(DWORD64 procId, const wchar_t* modName)
+{
+      uintptr_t modBaseAddr = 0;
+      HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, (DWORD)procId);
+      if (hSnap != INVALID_HANDLE_VALUE) {
+            MODULEENTRY32W modEntry;
+            modEntry.dwSize = sizeof(modEntry);
+            if (Module32FirstW(hSnap, &modEntry)) {
+                  do {
+                        if (!_wcsicmp(modEntry.szModule, modName)) {
+                              modBaseAddr = (uintptr_t)modEntry.modBaseAddr;
+                              break;
+                        }
+                  } while (Module32NextW(hSnap, &modEntry));
+            }
+      }
+      CloseHandle(hSnap);
+      return modBaseAddr;
+}
+
+// This music play detecting logic is taken from Chuniwide--it works pretty well
+static DWORD WINAPI Listen(LPVOID)
+{
+      auto gameplayflag = false;
+      auto procID = GetCurrentProcessId();
+      auto addr1 = GetModuleBaseAddress(procID, L"chusanApp.exe") + 0x1EE6AF8;
+      auto commaflag = false;
+
+      std::ostringstream oss;
+      while (1) {
+            auto gameplayread = *((std::uint32_t*)addr1);
+
+            if (gameplayread > 0 && gameplayflag == false) {
+                  gameplayflag = true;
+
+                  // Skip loading time, skill activation etc.
+                  Sleep(12000);
+
+                  commaflag = false;
+                  oss << "{\n\t\"scoreGraph\": [\n\t\t";
+            }
+
+            if (gameplayread == 0 && gameplayflag == true) {
+                  gameplayflag = false;
+
+                  oss << "\n\t]\n}";
+
+                  std::ofstream ofs("score-" + std::to_string(time(nullptr)) + ".json");
+                  ofs << oss.str();
+
+                  oss.str(std::string{});
+            }
+
+            if (gameplayflag == 0) {
+                  Sleep(100);
+            } else {
+                  auto val = 1010000 - g_current_score;
+                  if (commaflag) {
+                        oss << ", ";
+                  } else {
+                        commaflag = true;
+                  }
+                  oss << val;
+            }
+            Sleep(1000);
+      }
+      return 0;
+}
+
+BOOL WINAPI DllMain(HINSTANCE hinst, DWORD dwReason, LPVOID reserved) 
+{
+      if (DetourIsHelperProcess()) {
+            return TRUE;
+      }
+
+      if (dwReason == DLL_PROCESS_ATTACH) {
+            DetourRestoreAfterWith();
+
+            DetourTransactionBegin();
+            DetourUpdateThread(GetCurrentThread());
+            auto rv = DetourAttach(&(PVOID&)OrigGetLostScore, GetLostScore);
+            if (rv != NO_ERROR) {
+                  std::ofstream ofs("grapher.txt");
+                  ofs << "DetourAttach failed: " << rv << std::endl;
+                  return FALSE;
+            }
+            DetourTransactionCommit();
+
+            CreateThread(nullptr, 0, Listen, nullptr, 0, nullptr);
+      }
+      return TRUE;
+}
